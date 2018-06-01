ffe575a4e5732029abf4d53612ff6d1842db32f4
Alexander Zaytsev <hazzik@gmail.com>
2018-06-01 12:19:43 +1200

    Initial version

---
+++ src/SpanBridge.Tests/StringBuilderExtensionsTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using NUnit.Framework;
+
+namespace SpanBridge.Tests
+{
+    [TestFixture]
+    public class StringBuilderExtensionsTest
+    {
+        [Test]
+        public void CanAppendToStringBuilder()
+        {
+            var sb = new StringBuilder();
+            sb.Append("Hello World".AsSpan());
+            Assert.That(sb.ToString(), Is.EqualTo("Hello World"));
+        }
+
+        [Test]
+        public void CanInsertToStringBuilder()
+        {
+            var sb = new StringBuilder("World");
+            sb.Insert(0, "Hello ".AsSpan());
+            Assert.That(sb.ToString(), Is.EqualTo("Hello World"));
+        }
+
+        [Test]
+        public void CheckEquals()
+        {
+            var sb = new StringBuilder("Hello World");
+            var equals = sb.Equals("Hello World".AsSpan());
+            Assert.That(equals);
+        }
+        
+        [Test]
+        public void CheckNotEquals()
+        {
+            var sb = new StringBuilder("Hello World!");
+            var equals = sb.Equals("Hello World1".AsSpan());
+            Assert.That(equals, Is.Not.True);
+        }
+    }
+}
---
+++ src/SpanBridge/System/Text/StringBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿#if !NETCOREAPP2_1
+using System.Runtime.InteropServices;
+
+namespace System.Text
+{
+    public static class StringBuilderExtensions
+    {
+        public static unsafe StringBuilder Append(this StringBuilder @this, ReadOnlySpan<char> value)
+        {
+            if (value.Length > 0)
+            {
+                fixed (char* c = &MemoryMarshal.GetReference(value))
+                {
+                    @this.Append(c, value.Length);
+                }
+            }
+
+            return @this;
+        }
+
+        public static unsafe StringBuilder Insert(this StringBuilder @this, int index, ReadOnlySpan<char> value)
+        {
+            if (index > @this.Length)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (value.Length > 0)
+            {
+                fixed (char* c = &MemoryMarshal.GetReference(value))
+                {
+                    @this.Insert(index, new string(c, 0, value.Length));
+                }
+            }
+
+            return @this;
+        }
+
+        public static bool Equals(this StringBuilder @this, ReadOnlySpan<char> span)
+        {
+            if (span.Length != @this.Length)
+                return false;
+
+            return span.Equals(@this.ToString().AsSpan(), StringComparison.Ordinal);
+        }
+    }
+}
+#endif
+
